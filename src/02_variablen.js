89a748d528d9cc73342bf7197716a8b1280d7b24
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-01-09 12:07:23 +0000

    02 / Variablen vs. Konstanten

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -24,19 +24,31 @@
     JS ist ein untypisierte Sprache!
 */
 
-let test; // Deklaration
-console.log("Datentyp: " + typeof test);
-console.log("Inhalt: " + test);
-console.log("-------------");
-test = "hello"; // string
-console.log("Datentyp: " + typeof test);
-console.log("Inhalt: " + test);
-console.log("-------------");
-test = 2; // number
-console.log("Datentyp: " + typeof test);
-console.log("Inhalt: " + test);
-console.log("-------------");
-test = true;
-console.log("Datentyp: " + typeof test);
-console.log("Inhalt: " + test);
+// let test; // Deklaration
+// console.log("Datentyp: " + typeof test);
+// console.log("Inhalt: " + test);
+// console.log("-------------");
+// test = "hello"; // string
+// console.log("Datentyp: " + typeof test);
+// console.log("Inhalt: " + test);
+// console.log("-------------");
+// test = 2; // number
+// console.log("Datentyp: " + typeof test);
+// console.log("Inhalt: " + test);
+// console.log("-------------");
+// test = true;
+// console.log("Datentyp: " + typeof test);
+// console.log("Inhalt: " + test);
 
+/* Variablen vs. Konstanten in JS */
+
+// Variable
+let test;  // Variable : Deklaration
+test = "hi"; // Wertzuweisung (Initialisierung)
+test = "hello"; // Überschreiben
+console.log("Inhalt: " + test); // Ausgabe
+
+// Konstante
+const test2 = "Hi!";  // Deklaration + Wertzuweisung gemeinsam !
+test2 = "Hello!"; // Laufzeitfehler !!
+console.log("Inhalt: " + test2); // Ausgabe
