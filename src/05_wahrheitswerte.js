78737ed86742ae2440e7e466aaf1845abf533c02
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-01-10 11:56:33 +0000

    05 / Variante mit Date()

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -7,11 +7,14 @@ const birthYearJohn = 2000;
 const birthYearMark = 1990;
 
 // Berechnung Alter
-ageJohn = 2024 - birthYearJohn;
-ageMark = 2024 - birthYearMark;
+let date = new Date(); // Systemfunktion Datum
+let year = date.getFullYear(); // Daten liefern lassen!!
+ageJohn = year - birthYearJohn;
+ageMark = year - birthYearMark;
 
 // Ausgabe
 console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
+console.log("Datum: " + date);
 
 
