dacf51d8511dda87382c4975a35130faeba12262
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-01-11 11:30:45 +0000

    06 / SWITCH|CASE 2

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -71,32 +71,49 @@ isJohnEqual = (ageJohn == ageMark);
 
 /****** Fallunterscheidung / SWITCH|CASE 1 ******/
 
-const firstName = "Jane";
-let job;
+// const firstName = "Jane";
+// let job;
 
-job = "driver";  // .. fährt TAXI! / UBER
-job = "diver"; // .. taucht im Rhein! 
-job = "artist"; // .. malt ein Bild!
-job = "pilot"; // .. macht etwas anderes! --> default
-job = "teacher"; // .. unterrichtet!
-job = "instructor"; // .. unterrichtet!
+// job = "driver";  // .. fährt TAXI! / UBER
+// job = "diver"; // .. taucht im Rhein! 
+// job = "artist"; // .. malt ein Bild!
+// job = "pilot"; // .. macht etwas anderes! --> default
+// job = "teacher"; // .. unterrichtet!
+// job = "instructor"; // .. unterrichtet!
 
-switch (job) 
-{
-    case "driver":
-        console.log(firstName + " fährt TAXI!");
+// switch (job) 
+// {
+//     case "driver":
+//         console.log(firstName + " fährt TAXI!");
+//         break;
+//     case "diver":
+//         console.log(firstName + " taucht im Rhein!");
+//         break;
+//     case "artist":
+//         console.log(firstName + " malt ein Bild!");
+//         break;
+//     case "teacher":
+//     case "instructor":
+//         console.log(firstName + " unterrichtet!");
+//         break;
+//     default:
+//         console.log("Jane macht etwas anderes!");
+//         break;
+// }
+
+/********  Fallunterscheidung / SWITCH|CASE 2 *******/
+
+const cond = true;
+const value = 2;
+
+switch (cond) {
+    case (value == 1):
+        console.log("Value hat den Wert 1.");
         break;
-    case "diver":
-        console.log(firstName + " taucht im Rhein!");
-        break;
-    case "artist":
-        console.log(firstName + " malt ein Bild!");
-        break;
-    case "teacher":
-    case "instructor":
-        console.log(firstName + " unterrichtet!");
+    case (value == 2):
+        console.log("Value hat den Wert 2.");
         break;
     default:
-        console.log("Jane macht etwas anderes!");
+        console.log("Value hat einen anderen Wert.");
         break;
 }
