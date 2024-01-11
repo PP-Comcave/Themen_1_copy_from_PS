5e813a5dd7d132d49f6ac758f38a8de7afba64c2
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-01-11 08:23:57 +0000

    05 / Tests

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -3,7 +3,7 @@
 
 // Deklaration
 let ageJohn, ageMark;
-const birthYearJohn = 2000;
+const birthYearJohn = 1980;
 const birthYearMark = 1990;
 
 // Berechnung Alter
@@ -15,6 +15,12 @@ ageMark = year - birthYearMark;
 // Ausgabe
 console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
-console.log("Datum: " + date);
+// console.log("Datum: " + date);
 
+// Deklaration
+let isJohnOlder  // Is John older? --> y/n --> true / false
+isJohnOlder = (ageJohn > ageMark); // Test
+isJohnEqual = (ageJohn == ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
 
