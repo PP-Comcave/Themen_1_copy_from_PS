e8774d3fea4c59cd36fbeb5a7bb89d732c133cda
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-01-11 09:48:42 +0000

    06 / IF-ELSE

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -1,7 +1,7 @@
 // Entscheidungsstrukturen | control flow
 
 // Deklaration + Assignment
-const ageJohn = 22;
+const ageJohn = 30;
 const ageMark = 30;
 
 // Deklaration
@@ -26,10 +26,23 @@ console.log("------------------");
 // if(true)
 // if(false)
 // if(isJohnOlder)
+// if(ageJohn > ageMark)
+// {
+//     console.log("John ist älter!");
+// }
+
+/************ IF - ELSE  ************/
+// mit Alternative  
+// entweder ja oder nein
+
+// if (true)
+// if (false)
+// if (isJohnOlder)
 if(ageJohn > ageMark)
 {
-    console.log("John is älter!");
+    console.log("John ist älter!");
+} 
+else 
+{
+    console.log("John ist jünger!");
 }
-
-
-
