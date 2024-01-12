673f357a83ab8e4a1fe25c8abbf3c5bdffe881e1
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-01-12 10:45:54 +0100

    Add files via upload

---
+++ src/08_funktionen_I.js
@@ -0,0 +1,27 @@
+/***** Funktionen 01 *****/
+// 1. Kapselung von Codeblöcken
+
+// Funktionsaufruf | call
+// test();
+
+// Funktionsrumpf | callee
+// Funktionsdeklaration
+function test()
+{ 
+    console.log("Hallo, Benjamin!");
+}
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+// Call
+// ausgabeNamen();
+
+// Funktion
+function ausgabeNamen() {
+    // interne Variable | What happens in VEGAS ...
+    let firstName = "Maria";
+    console.log("Hallo, " + firstName + "!");
+}
+
+// console.log(firstName); // Fehler : scope!
