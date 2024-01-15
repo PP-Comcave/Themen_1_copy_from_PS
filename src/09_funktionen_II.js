8c4ad671bb3467e3f96aa5b7c2c8330573f0129e
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-01-15 11:51:09 +0000

    09 / startApp() getNum() , getOp()

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -4,24 +4,44 @@
 /*
 0. a+b | a-b | a*b | a/b  --> Ergebnis c 
 
-1. Dateneingabe + -überprüfung :  
+1. Dateneingabe + -überprüfung :  check! / todo!
 2. Auswahl Rechenart : check!
 3. Fkt. Grundrechenarten : check!
 4. Ausgabe in Konsole : check!
 */
 
+const prompt = require('prompt-sync')({sigint: true});
+
 const ERROR_STR_DIV = "Division durch 0 ist nicht möglich!";
 const ERROR_STR_GEN = "Irgendwas ging schief!";
 
+startApp();
+function startApp() {
+	output(calculator(getNum1(),getNum2(),getOp()));
+}
+
+function getNum1() {
+	return parseInt(prompt("Zahl1?: ")) ;
+}
+
+function getNum2() {
+	return parseInt(prompt("Zahl2?: ")) ;
+}
+
+function getOp() {
+	return prompt("OP?: ");
+}
+
+
 // module: calculator | tests:
 // agreement : "+","-","*",":","/"
-output(calculator(3,2,"+"));
-output(calculator(3,2,"-"));
-output(calculator(3,2,"*"));
-output(calculator(3,2,":"));
-output(calculator(3,2,"/"));
-output(calculator(3,0,"/"));
-output(calculator(3,2,"#?!"));
+// output(calculator(3,2,"+"));
+// output(calculator(3,2,"-"));
+// output(calculator(3,2,"*"));
+// output(calculator(3,2,":"));
+// output(calculator(3,2,"/"));
+// output(calculator(3,0,"/"));
+// output(calculator(3,2,"#?!"));
 function calculator(a,b,op) {
 	switch (op) {
 		case "+": // add()
@@ -37,9 +57,6 @@ function calculator(a,b,op) {
 			return ERROR_STR_GEN;
 	}
 }
-
-
-
 
 // module: division a / b |  test:
 // output(divide(4,2));
