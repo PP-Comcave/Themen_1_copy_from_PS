2e9f06565870832861526805565fd86f9cfda260
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-01-15 09:33:36 +0000

    09 / multiply() ++

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -10,17 +10,23 @@
 4. Ausgabe in Konsole : check!
 */
 
+// module: multiplication a * b |  test:
+output(multiply(3,2));
+output(multiply(3,-2));
+output(multiply(3,0));
+function multiply(a,b) {
+	return  a * b;
+}
 
 
 // module: subtraction a - b |  test:
-output(subtract(3,2));
-output(subtract(3,-2));
-output(subtract(3,0));
-output(subtract(0,2));
+// output(subtract(3,2));
+// output(subtract(3,-2));
+// output(subtract(3,0));
+// output(subtract(0,2));
 function subtract(a,b) {
 	return a - b;
 }
-
 
 // module: addition a + b |  test:
 // output(add(2,2));
